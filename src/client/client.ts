c4235b712e56585c1a9f8d655dd98ebecfe5f383
pradeepdurai <pradeepdurai66@gmail.com>
2022-10-12 15:18:42 +0530

    3D model added

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,9 +7,11 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 const scene = new THREE.Scene()
 // scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.DirectionalLight(0xffffff,1);
-light.position.set(2,2,5);
+const light = new THREE.HemisphereLight(0xffeeb1, 0x080820, 1.5);
+// light.position.set(2,2,4);
+
 scene.add(light);
+
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -18,19 +20,26 @@ const camera = new THREE.PerspectiveCamera(
     1000
 )
 camera.position.z = 2
+camera.position.y = 1
+
+
 
 const loader = new GLTFLoader();
 
-loader.load('3dModels/Vessel.glb',GLTF, XHR);
+loader.load('3dModels/Vesselv2.glb',GLTF, XHR);
+let root:any
 function GLTF(GLTF:any){
-    let root = GLTF.scene;
-    root.scale.set(0.2,0.2,0.2)
+    root = GLTF.scene;
+    root.scale.set(0.18,0.18,0.18);
     scene.add(root);
+    console.log(GLTF)
 }
 function XHR(xhr:any){
-    console.log('XHR ->', (xhr.loader / xhr.total) * 100 , "% Loader" )
+    console.log('XHR ->', (xhr.loader / xhr.total) * 100)
 }
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer();
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 2.3;
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -52,6 +61,9 @@ document.body.appendChild(stats.dom)
 function animate() {
     requestAnimationFrame(animate)
     render()
+    // root.rotation.x += 0.01;
+    root.rotation.y += 0.01;
+
     stats.update()
 }
 
